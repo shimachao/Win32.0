916d99bcfe4cbca98537ebe9d1feb752afaee10a
chaoc <531236305@qq.com>
2016-03-20 19:53:44 +0800

    添加Base.h文件，将GDI+相关的声明都放在其中。

---
+++ Win32.0/Base.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Windows.h>
+#include <gdiplus.h>
+
+using Gdiplus::Graphics;
+using Gdiplus::Rect;
+using Gdiplus::RectF;
+using Gdiplus::GdiplusStartupInput;
+using Gdiplus::GdiplusShutdown;
+using Gdiplus::Pen;
+using Gdiplus::Color;
+using Gdiplus::Image;
+using Gdiplus::Brush;
+using Gdiplus::HatchBrush;
+using Gdiplus::HatchStyleHorizontal;
+using Gdiplus::FontFamily;
+using Gdiplus::StringFormat;
+using Gdiplus::SolidBrush;
+using Gdiplus::FontStyleBold;
+using Gdiplus::UnitPoint;
+using Gdiplus::StringAlignmentCenter;
+using Gdiplus::Matrix;
+using Gdiplus::PrivateFontCollection;
+using Gdiplus::FontStyleRegular;
+using Gdiplus::UnitPixel;
+using Gdiplus::PenAlignmentInset;
