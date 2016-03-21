9c7bb443629cac04fd262d8984f9214bb4d06aac
chaoc <531236305@qq.com>
2016-03-21 20:47:23 +0800

    测试文件

--- Win32.0/Test.cpp
+++ Win32.0/Test.cpp
@@ -1,8 +1,32 @@
 #include "Window.h"
+#include "AbsoluteLayout.h"
+#include "FlowLinearLayout.h"
+#include "StaticText.h"
+#include "Button.h"
+#include "Graph.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+    Graph g;
+
     Window window(600, 400);
+
+    FlowLinearLayout flowLayout(100, 400, FlowLinearLayout::vertical);
+
+    StaticText staticText(100, 50, L"静态文本框");
+    flowLayout.add(&staticText);
+
+    Button button(100, 50, L"按钮1");
+    flowLayout.add(&button);
+
+    Button button2(100, 50, L"按钮2");
+    flowLayout.add(&button2);
+
+    AbsoluteLayout absLayout(600, 400);
+    absLayout.add(&flowLayout, 300, 0);
+
+    window.setLayout(&absLayout);
+
     window.Initialize();
     window.Run();
 
